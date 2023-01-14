23ab9c307ea72428c977c752f6c9687dfecefd37
Roopa Karajagi <Roopa.Karajagi@sony.com>
2023-01-14 10:33:33 +0530

    Chater-4 ShowMe the Code

--- App.js
+++ App.js
@@ -1,135 +1,231 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Logo_Image from "./images/logo.jpg";
+import { swiggy_data as RestaurantList } from "./data";
 
-/*
-PARCEL ---
-* HMR - Hot module reloading
-* File watcher algorith - written in C++
-* BUNDLING
-* MINIFY
-*CLEANING UP THE CODE //REMOVING CONSOLES
-* DEV AND PRODUCTION BUILD --     npx parcel build (start file) , npx parcel build (start file)
-* SUPER FAST BUILD ALGORITHMS
-* IMAGE OPTIMIZATION
-* CACHING MECHANISM WHILE DEVELOPMENT
-* port number is taken care by parcel
-* https on dev -- we can not use , google doesn't trust locahost
-* Compatible with older version of browser
-* Consistent Hashing Alogorithm
-* Zero config
-* Tree shaking -- removing unwanted codes
-
-* In normal create react app by default react uses webpack and babel 
-* -----here we use parcel and babel 
-* Browselist -- which makes our app  compatible with different browser 
-
-* trasitive dependencies means --> not only dependent on parcel , it also depends on other packages
-*To run production build app , we need to add dependencies to run the application and this dependencies also dependent on other packages too so it is called trasitive dependencies - dendency Tree
-*/
+/***
+ *  - Header
+ *      - logo(Title)
+ *       - delivery location(optional)
+ *        - nav Items(right)
+ *         - cart
+ *  - Body
+ *      - Search Bar
+ *    - Reastaturant list
+ *         - Reastaurant Card(Information -- Many Cards)
+ *              -Image
+ *              -Name
+ *              - Rating
+ *              -cusines(food varieties)
+ *
+ *
+ *    - Footer
+ *      -- links
+ *      -- social Icons
+ *      -- copyright
+ *
+ */
 
 // React.createElement ==> Object ==> render HTML(DOM)
 // Key and Id is different : React keeps track of key to comapre the elements
 
-//Adding multiple elements inside it , it will become messy
-const container = React.createElement(
-  "div",
-  { id: "container", hellow: "Roopa" },
-  [
-    React.createElement(
-      "h1",
-      { style: { color: "blue" }, key: "1", id: "title1" },
-      "Namaste React Live!!!"
-    ),
-    React.createElement(
-      "h2",
-      { style: { color: "green" }, key: "2", id: "title2" },
-      "Hurray , Let's begin with New Beginnings"
-    ),
-    React.createElement("h3", { id: "title3", key: "3" }, "Next Level"),
-  ]
-);
-//Adding ul Tag
-
-const unOrderList = React.createElement(
-  "ul",
-  { id: "unorder", hellow: "Roopa" },
-  [
-    React.createElement("li", { key: "l1" }, "Home"),
-    React.createElement("li", { key: "l2" }, "About"),
-    React.createElement("li", { key: "l3" }, "Contact"),
-  ]
-);
-
-// JSX: 
-// JSX ELement  -- It is not HTML inside Javascript, its HTML -like syntax
-// JSX --> React.createElement -->Object --> HTML(DOM) --> it is converted by babel
-//  React Element in JSX -- JSX expression -- its JSX syntax
-const heading = <h1 key="1">Namaste React</h1>;
-const heading2 = (
-  <h2 key="h_2" id="title">
-    Hurray!! Live Class
-  </h2>
-);
-// console.log("heading2" , heading2)
-//we are creating react root using React.createRoot
-/********************************************* */
-
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// passing an react element inside the root
-// async defer
-/********************************************* */
-
-// root.render([heading , heading2]);
-
-/********************************************* */
-// FUnctional Component -- Normal javascript Function itself
-const Title =()=>(
-  <h1 id="title_1">
-    Life Lines
-  </h1>
-)
-
-// we can write  functional component in different types
-const HeaderComponent = () =>{
-  return (
-    <div>
-      <Title />
-      {/* {Title()} */}
-      <h1>Rise Up</h1>
-      <h2>Life is Made for Living</h2>
-    </div>
-  )
-}
-const HeaderComponent1 =() =>(
-  <div>
-    <h1>Rise Up</h1>
-    <h2>Focus</h2>
+//Title of the APp
+const Title = () => (
+  <div className="title_comp">
+    <a href="/">
+      <img className="logo" src={Logo_Image} alt="Food villa" />
+    </a>
+    <h1 className="title" id="title">
+      {" "}
+      Food Villa
+    </h1>
   </div>
-)
-function HeaderComponent2() {
+);
+const HeaderComponent = () => {
   return (
-    <div>
-      <h3>Go and Learn New Things</h3>
+    <div className="header_comp">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact </li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  )
-}
-const HeaderComponent3 = function() {
+  );
+};
+// config Driven UI:
+
+const config = [
+  {
+    type: "carousel",
+    cards: [
+      {
+        offerName: "50% off",
+      },
+      {
+        offerName: "No Delivery Charge",
+      },
+    ],
+  },
+  {
+    type: "restaurants",
+    cards: [
+      {
+        name: "Burger King",
+        image:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gyiuf0vilqvtuhvz77vb",
+        cusines: ["Burger", "Amarican"],
+        rating: "4.4",
+      },
+      {
+        name: "KFC",
+        image:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gyiuf0vilqvtuhvz77vb",
+        cusines: ["Burger", "Amarican"],
+        rating: "4.4",
+      },
+    ],
+  },
+];
+// const burgerKing = {
+//   name: "Burger King",
+//   image:
+//     "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gyiuf0vilqvtuhvz77vb",
+//   cusines: ["Burger", "Amarican"],
+//   rating: "4.4",
+// };
+
+//props -- properties
+
+//arguments and parameters
+// function fn (params1 , pass2){
+
+// }
+// fn(args1 , args2);
+
+// First Method
+
+//Using destructuring props
+/******
+ * when we call the components like this , we can destructure the data and use it like this
+ * <RestaurantCard1 restaurant ={RestaurantList[3]}/>
+ * 
+ * 
+    const RestaurantCard1 = ({restaurant}) => {
+  const {name , cuisines , cloudinaryImageId , avgRating} = restaurant.data
+  console.log(name, cuisines , cloudinaryImageId , avgRating); --> data comes as expected
+  };
+  *****/
+
+// second Method
+
+//property destructuring will not work directly
+// when we call the component like this and it has child properties
+/*****
+ * 
+ * <RestaurantCard1 restaurant ={RestaurantList[0].data}/>
+ * 
+ * 
+const RestaurantCard = ({name , cuisines , cloudinaryImageId , avgRating }) => {
+console.log(name, cuisines , cloudinaryImageId , avgRating); //---> undefined
+};
+ */
+
+// 3rd Method
+//individual destructring of properties and this work when we call our component as
+/***
+ * 
+ * <RestaurantCard
+name={RestaurantList[0].data.name}
+cuisines={RestaurantList[0].data.cuisines}
+cloudinaryImageId={RestaurantList[0].data.cloudinaryImageId}
+avgRating={RestaurantList[0].data.avgRating}
+/> 
+
+const RestaurantCard = ({name , cuisines , cloudinaryImageId , avgRating }) => {
+  console.log(name, cuisines , cloudinaryImageId , avgRating); //---> data comes as expected
+  };
+
+***/
+
+// 4th Method
+// using spread operator and destructuring properties
+/****
+   *spread all the properties of data and use it in component
+   * Componenet Calling
+   * 
+   *  <RestaurantCard
+          {...RestaurantList[0].data}
+        />
+        <RestaurantCard
+           {...RestaurantList[1].data}
+        />
+        <RestaurantCard
+           {...RestaurantList[2].data}
+        />
+
+        *****render of component********
+
+        const RestaurantCard = ({name , cuisines , cloudinaryImageId , avgRating }) => {
+        console.log(name, cuisines , cloudinaryImageId , avgRating); //---> data comes as expected
+  };
+   */
+
+const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
+  // console.log(name, cuisines, cloudinaryImageId, avgRating); //---> data comes as expected
   return (
-    <div> 
-      <h4>Dedicate soul and time</h4>
+    <div className="card">
+      <a href="/" className="card-link">
+        <div className="card-details">
+          <div className="img_card">
+            <img
+              src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/${cloudinaryImageId}`}
+              width={254}
+              height={160}
+              alt="card_image"
+            />
+          </div>
+          <h2>{name}</h2>
+          <h3>{cuisines.join(", ")}</h3>
+          <h4>{avgRating}</h4>
+        </div>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<>
-  <HeaderComponent />
-  <HeaderComponent1/>
-  <HeaderComponent2 />
-  <HeaderComponent3 />
- </> )
+//it is like passing an arguments to functions and can be used as params in our function
+const Body = () => {
+  return (
+    <div className="rest_body">
+      <div className="rest_header">Restaurants</div>
+      <div className="rest_list">
+        <div className="card-group">
+          {RestaurantList.map((item, index) => {
+            return <RestaurantCard {...item.data} key={item.data.id} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// BUNDLER
-// webpack
-// vite , parcel
+const Footer = () => {
+  return <h5>Footer</h5>;
+};
+//First structure a layout
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
