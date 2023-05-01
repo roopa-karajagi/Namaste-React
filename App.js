ef6566c7824de31bc954b9a79cac3d8e40a33e12
N Karajagi <rnkarajagi.ext@deloitte.com>
2023-05-01 19:45:16 +0530

    stage1 changes

--- App.js
+++ App.js
@@ -10,12 +10,59 @@ const heading = React.createElement('h1' , {
 } , "Namaste React!!!");
 console.log(heading);
 
+
+//core of react --> where we write our logic to be displayed on ui
 const heading1 = React.createElement('h1' ,{style: {color:'blue'}} ,"Heading1");
 const heading2 = React.createElement('h2' , {style: {color: 'green'}} ,"Heading2");
 const container = React.createElement('div' , {id : "container" }, [heading1 , heading2]);
 console.log(container);
 //we are creating react using React.createRoot
+//we are using ReactDOM --> this is where we are making changes to the DOM --> ReactDom is the one which  display our changes to the DOM and on UI
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 // passing an react element inside the root
-heading.innerHTML= "Namaste JS!!!";
-root.render(heading);         
+//injecting all react into the application through root render.
+root.render(container);
+
+
+//React Element is an Object
+// {
+//     "type": "div",
+//     "key": null,
+//     "ref": null,
+//     "props": {
+//         "id": "container",
+//         "children": [
+//             {
+//                 "type": "h1",
+//                 "key": null,
+//                 "ref": null,
+//                 "props": {
+//                     "style": {
+//                         "color": "blue"
+//                     },
+//                     "children": "Heading1"
+//                 },
+//                 "_owner": null,
+//                 "_store": {}
+//             },
+//             {
+//                 "type": "h2",
+//                 "key": null,
+//                 "ref": null,
+//                 "props": {
+//                     "style": {
+//                         "color": "green"
+//                     },
+//                     "children": "Heading2"
+//                 },
+//                 "_owner": null,
+//                 "_store": {}
+//             }
+//         ]
+//     },
+//     "_owner": null,
+//     "_store": {}
+// }
+
+         
